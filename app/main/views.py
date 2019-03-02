eb9d9e31293244553b793e08a7240690294efe5e
Pnshimiye <pnshimiye@gmail.com>
2019-03-02 09:52:22 +0200

    clean up code

--- app/main/views.py
+++ app/main/views.py
@@ -26,18 +26,16 @@ def index():
 @login_required
 def new_pitch():
     form = PitchForm()
-    # Pitch = pitch.Pitch
-    # movie = get_movie(id)
+  
 
-    if form.validate_on_submit():
-        # title = form.title.data
+    if form.validate_on_submit():    
         teaser = form.teaser.data
         pitch = form.pitch.data
         new_pitch = Pitch(user_id=current_user.id,teaser=teaser, pitch=pitch)
         new_pitch.save_pitch()
         return redirect(url_for('.index',pitch = pitch ))
 
-    # username = f'{user.username} pitch'
+   
     return render_template('new_pitch.html', pitch_form=form)
 
 
@@ -70,59 +68,7 @@ def new_comment(id):
 
 
 
-# @main.route('/user/<uname>/update',methods = ['GET','POST'])
-# @login_required
-# def update_profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if user is None:
-#         abort(404)
-
-#     form = UpdateProfile()
-#     user.pitch = form.pitch.data
-       
-
-#     if form.validate_on_submit():
-#         db.session.add(user)
-#         db.session.commit()
-       
-
-    
-
-#         return redirect(url_for('.profile',uname=user.username))
-
-#     return render_template('profile/update.html',form =form)
-
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
-
-# @main.route('/user/<uname>/update/pitcf',methods= ['POST'])
-# @login_required
-# def update_pitch(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if pitch:
-#         # filename = photos.save(request.files['photo'])
-#         # path = f'photos/{filename}'
-#         # user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
-
-# @main.route('/')
-# def index():
-#     """ View root page function that returns index page
-#     """
-     
-
-#     title = 'Home- Welcome'
-#     return render_template('index.html', title = title, )
-
+ 
 
 
 
